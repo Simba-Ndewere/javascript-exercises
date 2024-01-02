da20ce58b87f667778defbcf8ec82174a3338c98
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-02 19:38:43 +0000

    pass tests in tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempA) {
+  let temp = 5/9 * (tempA-32);
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempA) {
+  let temp = tempA * (9/5) + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
