ec31ebabbf15af00b9a817949edefa2e43e1facd
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-02 18:31:04 +0000

    pass all tests in sumAll.spec.ts

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let total = 0;
+    if(num1 < 0 || num2 < 0) return 'ERROR';
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
 
+    if(num1 < num2){
+        while(num1<=num2){
+            total+=num1;
+            num1++;
+        }
+    }else{
+        while(num2<=num1){
+            total+=num2;
+            num2++;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
