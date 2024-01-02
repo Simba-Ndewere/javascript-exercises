992c6113e95693d0a6c0deee6022843607d130cd
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-02 00:39:34 +0000

    passed tests for javascript tests on helloWorld.spec.js and repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word,number) {
+    let finalWord = '';
 
+    if(number < 0){
+        return 'ERROR';
+    }
+
+    for(let a = 0; a < number; a++){
+        finalWord = finalWord + word;
+    }
+    return finalWord;
 };
 
 // Do not edit below this line
