0cf346d0c77bc7408f49171a50ac6712fad4c6fc
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-02 01:06:28 +0000

    pass tests for tests in reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = '';
+    for(let a = word.length-1; a > -1; a--){
+        reversedWord = reversedWord + word[a];
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
