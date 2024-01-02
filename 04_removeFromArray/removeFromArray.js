5eb4c886a9833cfd1c64079571b96d0c90ca193f
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-02 17:48:08 +0000

    pass all tests in removeArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    for(let a = 1; a < arguments.length; a++){
+        if(!arguments[0].includes(arguments[a])){
+            continue;
+        }
+        let indexOfNumber = arguments[0].indexOf(arguments[a]);
+        arguments[0].splice(indexOfNumber,1);
+    }
+    console.log(arguments[0]);
+    return arguments[0];
 };
 
 // Do not edit below this line
