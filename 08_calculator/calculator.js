1ab79697752495d83a9b4b838ba44a89c81b4ccc
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-08 22:39:13 +0000

    pass all calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
 const add = function() {
-	
+  return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+  return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+  let b = 0;
+  let arrayArg = arguments[0];
+
+	for(let a = 0; a < arrayArg.length; a++){
+    b = b + arrayArg[a];
+  }
+  return b;
 };
 
 const multiply = function() {
+  let arrayArg = arguments[0];
+  let b = arrayArg[0];
 
+	for(let a = 1; a < arrayArg.length; a++){
+    b = b * arrayArg[a];
+  }
+  return b;
 };
 
 const power = function() {
-	
+  let a = arguments[0];
+  for(let b = 0; b < arguments[1]-1; b++){
+    a = a * arguments[0];
+  }
+  return a;
 };
 
 const factorial = function() {
-	
+	if(arguments[0] === 0) return 1;
+  let a = arguments[0];
+  for(let b = arguments[0]-1; b > 0; b-- ){
+    a = a * b;
+  }
+  return a;
 };
 
 // Do not edit below this line
