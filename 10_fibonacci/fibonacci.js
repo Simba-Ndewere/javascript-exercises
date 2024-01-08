c0c0ce12a58874640545b984d72c430e3d3a5d97
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-08 23:38:44 +0000

    pass fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = function() {
-
+    if(arguments[0] < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for(let counter = 0; counter <= arguments[0]; counter++){
+        c = a + b;
+        if(counter==arguments[0]){
+            return a;
+        }
+        a = b;
+        b = c;
+    }
 };
 
 // Do not edit below this line
