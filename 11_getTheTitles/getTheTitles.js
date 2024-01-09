8b40669399887a14a74f5a942ce1032c12ba40aa
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-09 01:36:16 +0000

    pass get titles tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let array = [];
+    for(let a = 0; a < books.length; a++){
+        array.push(books[a].title);
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
