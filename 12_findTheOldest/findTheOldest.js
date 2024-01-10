2c11cbdbbc4e247efd7c459e8b07242da1490c46
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-10 13:23:52 +0000

    pass findTheOldest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(objectsArray) {
+    let oldest = '';
+    let currentAge = 0;
+    const person = {
+        name : ''
+    };
+    for(let a = 0; a < objectsArray.length; a++){
+        let yearOfDeath = objectsArray[a].yearOfDeath;
+        let yearOfBirth =  objectsArray[a].yearOfBirth;
 
+        if(yearOfDeath === undefined){
+            yearOfDeath = new Date().getFullYear();
+        }
+   
+        if( yearOfDeath - yearOfBirth > currentAge){
+            currentAge = yearOfDeath - yearOfBirth;
+            oldest = objectsArray[a].name;
+        }
+    }
+    person.name = oldest;
+    return person;
 };
 
 // Do not edit below this line
